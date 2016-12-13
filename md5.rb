e43d1627898ed5c5ca347d72eddd7d478d85355b
Jeff Fowler <jeffowler@gmail.com>
2016-12-13 15:22:22 -0500

    init

--- md5.rb
+++ md5.rb
@@ -1,84 +1,79 @@
-require 'digest'
-@input = "This is my test string."
-# @input = "The quick brown fox jumps over the lazy dog."
-Digest::MD5.hexdigest(@input) # "d4108e090956a15e38e1b15151004086"
-
-@input.bytesize * 8
-
-@input.bytes.map {|b| "%08d" % b.to_s(2)}.join.length
-@arr = @input.bytes
-length = @arr.length
-@arr << 0b10000000
-@arr << 0b00000000 until @arr.length % 64  == 56
-
-# loooool
-@arr  << ("%064d" % (length * 8).to_s(2)).split('').each_slice(8).to_a.map {|e| e.join.to_i(2)}.reverse
-@arr.flatten!
-
-k = []
-(0.. 63).each do |i|
-    k[i] = (2**32 * Math.sin(i + 1).abs).floor
-end
-
-s = []
-s[ 0..15] = [ 7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22 ]
-s[16..31] = [ 5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20 ]
-s[32..47] = [ 4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23 ]
-s[48..63] = [ 6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21 ]
-
-def bitize(input)
-    input[0..3].reduce {|acc, e| acc << 32 | e}
-end
-def bitize_print(input)
-   (input[0..3].reduce {|acc, e| acc << 32 | e} % 2 ** 128 ).to_s(16)
-end
-
-def leftrotate(x, c)
-    ((x << c) | (x >> (32 - c))) & 0xffffffff 
-end
-
-# p "%032d" % [1,1,1,1].reduce {|acc, e| acc << 8 | e}.to_s(2)
-# p  "%032d" % ((1 << 8) | 1).to_s(2)
-
-table = [
-    0x67452301,
-    0xefcdab89,
-    0x98badcfe,
-    0x10325476
-]
-
-def F(x,y,z)    (x & y) | (~x & z) end
-def G(x,y,z)    (x & z) | (y & ~z) end
-def H(x,y,z)    x ^ y ^ z          end
-def I(x,y,z)    y ^ (x | ~z)       end
-
-a, b, c, d = table
-(0...64).each do |i|
-    if 0 <= i && i <= 15
-        f = F(b,c,d)
-        g = i
-    elsif 16 <= i && i <= 31
-        f = G(b,c,d)
-        g = (5 * i + 1) % 16
-    elsif 32 <= i && i <= 47
-        f = H(b,c,d)
-        g = (3 * i + 5) % 16
-    elsif 48 <= i && i <= 63
-        f = I(b,c,d)
-        g = (7 * i) % 16
+class Array
+    def to_int(x = 8)
+        self.each_with_index.map{|e,i| e << (x* i)}.reduce{|acc, e| acc | e}
     end
-    dTemp = d & 0xffffffff
-    d = c & 0xffffffff
-    c = b & 0xffffffff
-    b = (b + leftrotate(a + f + k[i] + @arr[g], s[i])) & 0xffffffff
-    a = dTemp
+end
+require 'digest'
+
+@rotate_amounts = [ 7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,
+                    5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,
+                    4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,
+                    6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21 ]
+
+@constants = (0.. 63).map { |i| (2**32 * Math.sin(i + 1).abs).floor }
+
+@init_values = [ 0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476 ]
+
+one = ->(b,c,d){ (b & c) | (~b & d) }
+two = ->(b,c,d){ (d & b) | (~d & c) }
+three = ->(b,c,d){ b ^ c ^ d }
+four = ->(b,c,d){ c ^ (b | ~d) }
+@functions = [
+    (0..15).map{ one },
+    (16..31).map{ two },
+    (32..47).map{ three },
+    (48..63).map{ four }
+].flatten
+
+one = ->(i){ i }
+two = ->(i){ (5 * i + 1) % 16 }
+three = ->(i){ (3 * i + 5) % 16 }
+four = ->(i){ (7 * i) % 16 }
+@index_functions = [
+    (0..15).map{ one },
+    (16..31).map{ two },
+    (32..47).map{ three },
+    (48..63).map{ four }
+].flatten
+
+def left_rotate(x, amount)
+    x &= 0xffffffff
+    ((x << amount) | (x >> (32 - amount))) & 0xffffffff
 end
 
+def md5(message)
+    message = message.bytes
+    orig_len_in_bits = (8 * message.length) & 0xffffffffffffffff
+    message << 0x80
+    while message.length % 64 != 56
+        message << 0
+    end
+    (message <<
+        orig_len_in_bits
+        .to_s(16)
+        .rjust(32, '0')
+        .scan(/../)
+        .map{|e| e.hex}
+        .reverse).flatten!
 
-table[0] += a
-table[1] += b
-table[2] += c
-table[3] += d
+    hash_pieces = @init_values.clone
+    a, b, c, d = hash_pieces
+    chunk = message[0..64]
 
-p bitize_print table
-p Digest::MD5.hexdigest(@input) # "d4108e090956a15e38e1b15151004086"
+    (0..63).each do |i|
+        f = @functions[i].call(b, c, d)
+        g = @index_functions[i].call(i)
+        to_rotate = a + f + @constants[i] + chunk[4*g...4*g+4].to_int
+        new_b = (b + left_rotate(to_rotate, @rotate_amounts[i])) & 0xFFFFFFFF
+        a, b, c, d = d, new_b, b, c
+    end
+
+    [a,b,c,d].each_with_index do |val, i|
+        hash_pieces[i] += val
+        hash_pieces[i] &= 0xFFFFFFFF
+    end
+    hash_pieces.to_int(32)
+end
+
+puts md5("This is my test string.").to_s(16).scan(/../).reverse.join
+
