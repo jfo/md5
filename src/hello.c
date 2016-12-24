218bbd14142f47f42c0d8a9b88073edf24778a29
Jeff Fowler <jeffowler@gmail.com>
2016-12-24 16:35:07 -0500

    moving constants into global

--- src/hello.c
+++ src/hello.c
@@ -8,6 +8,25 @@
 
 #define bitswap(NUM) ((NUM>>24)&0xff) | ((NUM<<8)&0xff0000) | ((NUM>>8)&0xff00) | ((NUM<<24)&0xff000000)
 
+unsigned long constants[64] =
+{ 0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+ 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+ 0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+ 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+ 0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+ 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+ 0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
+ 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+ 0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+ 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+ 0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
+ 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+ 0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
+ 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+ 0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+ 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391 };
+
+
 int main()
 {
     /* Load the source code containing the kernel*/
@@ -50,19 +69,26 @@ int main()
     cl_kernel kernel = NULL;
     kernel = clCreateKernel(program, "hello", &ret);
 
-    cl_mem inputmem = NULL;
-    inputmem = clCreateBuffer(context, CL_MEM_READ_WRITE, MEM_SIZE * sizeof(char), NULL, &ret);
-
     cl_mem outputmem = NULL;
     outputmem = clCreateBuffer(context, CL_MEM_READ_WRITE, MEM_SIZE * sizeof(char), NULL, &ret);
 
+    cl_mem inputmem = NULL;
+    inputmem = clCreateBuffer(context, CL_MEM_READ_WRITE, MEM_SIZE * sizeof(char), NULL, &ret);
     char inputbuffer[MEM_SIZE];
     sprintf(inputbuffer, "abbhdwsy%i", 0);
     ret = clEnqueueWriteBuffer(command_queue, inputmem, CL_TRUE, 0,
             MEM_SIZE * sizeof(char), inputbuffer, 0, NULL, NULL);
 
+    cl_mem constantsmem = NULL;
+    constantsmem = clCreateBuffer(context, CL_MEM_READ_ONLY, 64 * sizeof(long), NULL, &ret);
+
+    ret = clEnqueueWriteBuffer(command_queue, constantsmem, CL_TRUE, 0,
+            64 * sizeof(long), constants, 0, NULL, NULL);
+    printf("\n\nError Code: %i\n", ret);
+
     ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&outputmem);
     ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&inputmem);
+    ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&constantsmem);
 
     /* Execute OpenCL Kernel */
     ret = clEnqueueTask(command_queue, kernel, 0, NULL,NULL);
@@ -83,7 +109,6 @@ int main()
 
     printf("%s\n", outstr);
 
-    /* printf("\n\nError Code: %i\n", ret); */
 
     /* Finalization */
     ret = clFlush(command_queue);
